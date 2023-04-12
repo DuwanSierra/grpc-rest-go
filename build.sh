20cd2a86a3a724f084d11627a8b71c4956513d99
Duwan Sierra <desierrag@gmail.com>
2023-04-11 22:54:44 -0500

    add github actions  and docker files

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Build the project
+echo "Building the project..."
+./gradlew build jar
+# Finish the project
+echo "Finish the project..."
--- microservicea/src/main/java/com/grpc/microservicea/infrastructure/webclients/MicroserviceBRemoteClient.java
+++ microservicea/src/main/java/com/grpc/microservicea/infrastructure/webclients/MicroserviceBRemoteClient.java
@@ -1,5 +1,6 @@
 package com.grpc.microservicea.infrastructure.webclients;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
@@ -8,11 +9,13 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class MicroserviceBRemoteClient {
 
+    @Value("${microserviceb.url}")
+    private String microserviceBUrl;
     @Bean
     public WebClient microserviceBClient() {
         return WebClient
         .builder()
-        .baseUrl("http://localhost:8081/")
+        .baseUrl(microserviceBUrl)
         .exchangeStrategies(ExchangeStrategies
         .builder()
         .codecs(codecs -> codecs
